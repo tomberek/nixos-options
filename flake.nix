c85d9a7ee6e4b52d91206abec166562ade2778f8
Tom Bereknyei <tomberek@gmail.com>
2023-03-04 00:42:19 -0500

    fix: format

--- flake.nix
+++ flake.nix
@@ -3,35 +3,36 @@
   outputs = {
     self,
     nixpkgs,
-  }: 
-    let
-      processOption = value: let
-        res =
-          (value.type.getSubOptions [])
-          // {
-            description = value.description.text or value.description or "";
-            default = value.default or null;
-            example = value.example or null;
-          };
-      in
-        process (builtins.removeAttrs res ["_module"]);
-      process = {
-        "option" = value: processOption value;
-        "submodule" = value: processOption value;
-        "set" = value: let
-          res = builtins.mapAttrs (n: v: process v) value;
-        in
-          res;
-        "string" = value: value;
-        "lambda" = value: "lambda";
-        __functor = self: x: let
-          smartType = x._type or (builtins.typeOf x);
-        in
-          (self."${smartType}" or (_: _)) x;
-      };
+  }: let
+    processOption = value: let
+      res =
+        (value.type.getSubOptions [])
+        // {
+          description = value.description.text or value.description or "";
+          default = value.default or null;
+          example = value.example or null;
+        };
     in
-    process (import "${nixpkgs}/nixos" {
+      process (builtins.removeAttrs res ["_module"]);
+    process = {
+      "option" = value: processOption value;
+      "submodule" = value: processOption value;
+      "set" = value: let
+        res = builtins.mapAttrs (n: v: process v) value;
+      in
+        res;
+      "string" = value: value;
+      "lambda" = value: "lambda";
+      __functor = self: x: let
+        smartType = x._type or (builtins.typeOf x);
+      in
+        (self."${smartType}" or (_: _)) x;
+    };
+  in
+    process
+    (import "${nixpkgs}/nixos" {
       configuration = {};
       system = "x86_64-linux";
-    }).options;
+    })
+    .options;
 }
